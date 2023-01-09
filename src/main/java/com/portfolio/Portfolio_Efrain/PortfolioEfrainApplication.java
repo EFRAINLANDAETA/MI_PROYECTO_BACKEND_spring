c833bba89734224508cb7ef6634f1b3c03fea6f3
EFRAINLANDAETA <efrain_landaeta@hotmail.com>
2023-01-09 15:57:06 -0300

    mi primera subida Backend

---
+++ src/main/java/com/portfolio/Portfolio_Efrain/PortfolioEfrainApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Portfolio_Efrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioEfrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioEfrainApplication.class, args);
+	}
+
+}
